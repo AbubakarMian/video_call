2b162af954636b0ef0051419bf13e88b8259679f
AbubakarMian <abubakrmianmamoon@gmail.com>
2021-09-24 12:13:32 +0500

    test

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 const options = {
-  // key: fs.readFileSync("./key.pem"),
-  // cert: fs.readFileSync("./cert.pem"),
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
   // key: fs.readFileSync("/home2/developmenthatin/public_html/vc3/key.pem"),
   // cert: fs.readFileSync("/home2/developmenthatin/public_html/vc3/cert.pem"),
-  requestCert: false
+  // requestCert: false
   // debug: true,
 };
 const server = require("https").createServer(
@@ -16,8 +16,7 @@ const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
-    origin: 'https://development.hatinco.com:3031/*',
-    // origin: '*',
+    origin: '*',
   }
 });
 const { ExpressPeerServer } = require("peer");
