299bf3d2cb261157898bcf4e305d490acd3c6c94
AbubakarMian <abubakrmianmamoon@gmail.com>
2021-09-25 15:03:56 +0500

    test

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const options = {
   // requestCert: false
   // debug: true,
 };
-app.use(cors())
+// app.use(cors())
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", 
-  "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-  });
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", 
+//   "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+//   });
 
 const server = require("https").createServer(
   options,
