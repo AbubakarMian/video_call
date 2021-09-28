62c1db5d5bd24b72a3a9d8bbf3dc58d6b2274544
AbubakarMian <abubakrmianmamoon@gmail.com>
2021-09-28 19:41:23 +0500

    test

--- views/script.js
+++ views/script.js
@@ -19,13 +19,15 @@ showChat.addEventListener("click", () => {
   document.querySelector(".header__back").style.display = "block";
 });
 
+  var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443"
+  });
+
 const user = prompt("Enter your name");
 
-var peer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "443"
-});
+
 
 let myVideoStream;
 navigator.mediaDevices
